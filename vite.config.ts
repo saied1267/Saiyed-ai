5c7a60a20449a3bd0bbff6b3932667b8fc55336f
saied1267 <154495703+saied1267@users.noreply.github.com>
2026-01-19 17:06:51 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.API_KEY_2': JSON.stringify(process.env.API_KEY_2),
-    'process.env.API_KEY_3': JSON.stringify(process.env.API_KEY_3)
+    'process.env.API_KEY_3': JSON.stringify(process.env.API_KEY_3),
+    'process.env.API_KEY_4': JSON.stringify(process.env.API_KEY_3)
   },
   build: {
     outDir: 'dist',
