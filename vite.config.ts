535fc0e9b10341ae97cd6e9025672887c595d854
saied1267 <154495703+saied1267@users.noreply.github.com>
2026-01-19 22:21:47 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env.API_KEY_2': JSON.stringify(process.env.API_KEY_2),
-    'process.env.API_KEY_3': JSON.stringify(process.env.API_KEY_3),
-    'process.env.API_KEY_4': JSON.stringify(process.env.API_KEY_3)
+    // জেমিনি এপিআই কি সমুহ
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || ""),
+    'process.env.API_KEY_2': JSON.stringify(process.env.API_KEY_2 || ""),
+    'process.env.API_KEY_3': JSON.stringify(process.env.API_KEY_3 || ""),
+    'process.env.API_KEY_4': JSON.stringify(process.env.API_KEY_4 || ""),
+    
+    // ফায়ারবেস কনফিগারেশন সমুহ
+    'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY || ""),
+    'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN || ""),
+    'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID || ""),
   },
   build: {
     outDir: 'dist',
